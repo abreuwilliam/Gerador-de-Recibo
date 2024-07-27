da9112dd8f3db9ab6a322288e1e2476f32c45b1d
abreuwilliam <https://github.com/abreuwilliam>
2024-07-27 12:24:44 -0300

    pegando as variaveis em javascript

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 const formulario = document.querySelector("form");
 
 const ivalor = document.querySelector(".valor input");
-
+const inomepagador = document.querySelector(".nomepagador ");
+const icpfpagador = document.querySelector(".cpfpagador input");
+const ireferencia = document.querySelector(".referencia input");
+const inomeemissor = document.querySelector(".nomeemissor input");
+const icpfemissor = document.querySelector(".cpfemissor input");
+const itelefone = document.querySelector(".telefone input");
+const icidade = document.querySelector(".cidade input");
+const idata = document.querySelector(".data input");
 
 formulario.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    console.log(ivalor.value)
+   
+    console.log(idata.value)
+   
+    
     
     
 });
